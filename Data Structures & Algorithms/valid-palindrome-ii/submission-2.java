85af5fa53eadc37fa289e6c60b79e7ae9b6987c0
Punit Sharma <97792442+PunitMudgal@users.noreply.github.com>
2026-06-02 11:51:40 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s.substring(0, l) + s.substring(l + 1))
+                    || isPalindrome(s.substring(0, r) + s.substring(r + 1));
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+    static boolean isPalindrome(String s) {
+        int f = 0, e = s.length() - 1;
+        while (f <= e) {
+            if (s.charAt(f) != s.charAt(e)) {
+                return false;
+            }
+            f++;
+            e--;
+        }
+        return true;
+    }
+}
